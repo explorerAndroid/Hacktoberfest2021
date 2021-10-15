fbacb4f7e24c55bca8e991eeee9a593eacd3006d
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-10-15 14:09:05 +0530

    Reformatted Previous Python Codes and Added My New Python Codes

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,7 @@
 def is_leapyear(year):
-    if (year % 4) == 0:  # Checking for conditions for leap year viz. 4, 100, 400 and printing the output
+    if (
+        year % 4
+    ) == 0:  # Checking for conditions for leap year viz. 4, 100, 400 and printing the output
         if (year % 100) == 0:
             if (year % 400) == 0:
                 print(f"{year} is a leap year.\n")
@@ -12,5 +14,5 @@ def is_leapyear(year):
 
 
 while True:
-    year = int(input("Enter a year: \t"))   # Taking Input
+    year = int(input("Enter a year: \t"))  # Taking Input
     is_leapyear(year)  # Running the function
--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,20 @@
 def is_palindrome(text):
-    removed_whitespace = text.replace(" ", "").lower()    # Removing whitespace from string and converting it to lower case
-    reversed = removed_whitespace[::-1]     # Reversing the string and storing it in a separate variable
+    removed_whitespace = text.replace(
+        " ", ""
+    ).lower()  # Removing whitespace from string and converting it to lower case
+    reversed = removed_whitespace[
+        ::-1
+    ]  # Reversing the string and storing it in a separate variable
 
-    if removed_whitespace == reversed:    # Checking if original text is equal to reversed text, if it is then it is palindrome, else not.
+    if (
+        removed_whitespace == reversed
+    ):  # Checking if original text is equal to reversed text, if it is then it is palindrome, else not.
         print(f"Palindrome! {text} is a palindrome.\n")
 
     else:
-        print(f"Not A Palindrome! {text} is not a palindrome.\n")    
+        print(f"Not A Palindrome! {text} is not a palindrome.\n")
 
 
 while True:
-    text = input("Enter Some Text: \t")   # Taking input
-    is_palindrome(text)   # Running the function
+    text = input("Enter Some Text: \t")  # Taking input
+    is_palindrome(text)  # Running the function
